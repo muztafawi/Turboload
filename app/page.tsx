139d1c580d57f87448f1b7fc5ee87748472b5d94
Amin Mustafa <muztafawi@gmail.com>
2023-04-21 20:51:16 +0300

    Added input & button section

--- app/page.tsx
+++ app/page.tsx
@@ -4,5 +4,10 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <main>main part</main>;
+  return (
+    <main className="flex justify-center items-center align-middle h-fit">
+      <input type="url" name="" id="" />
+      <button>Download</button>
+    </main>
+  );
 }
