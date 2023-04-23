c9d832c6241e15a77cc717c488e6431e5dcfb7db
Amin Mustafa <muztafawi@gmail.com>
2023-04-23 21:54:39 +0300

    Styling input & button

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,13 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className="flex justify-center items-center align-middle h-fit">
-      <input type="url" name="" id="" />
-      <button>Download</button>
+    <main className="flex justify-center gap-3 items-center align-middle h-fit">
+      <input
+        type="url"
+        placeholder="Past url link"
+        className="outline-none bg-orange-500/25 text-sm py-3 px-4 rounded-xl"
+      />
+      <button className="bg-orange-500 py-2 px-4 rounded-md">Download</button>
     </main>
   );
 }
