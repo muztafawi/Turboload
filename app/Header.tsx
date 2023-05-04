36800c735dd81913cbc7d93dbbb55d62f73558c7
Amin Mustafa <muztafawi@gmail.com>
2023-05-04 22:01:54 +0300

    Centered main layout

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,20 +1,11 @@
-import { BeakerIcon, MoonIcon, SunIcon } from "@heroicons/react/24/solid";
-
 export default function Header() {
   return (
     <header className="flex justify-between items-center mx-auto py-4 px-8 mt-8 max-w-5xl">
       {/* Brand  */}
-      <div>
-        <h2 className="font-bold underline">
-          Turbo<span className="text-orange-400">load</span>
-        </h2>
-      </div>
 
-      {/* Dark & Light Mode Icons  */}
-      <div>
-        <SunIcon className="w-8 h-8 cursor-pointer text-orange-400" />
-        {/* <MoonIcon className="w-8 h-8 cursor-pointer text-gray-400" /> */}
-      </div>
+      <h2 className="font-bold underline text-gray-900 text-center">
+        Turbo<span className="text-orange-400">load</span>
+      </h2>
     </header>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,10 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <main className="flexLayout">
-      <input type="url" placeholder="Past url link" className="inputDesign" />
-      <button className="bg-orange-500 py-2 px-4 rounded-md">Download</button>
+      <div>
+        <input type="url" placeholder="Past url link" className="inputDesign" />
+        <button className="bg-orange-500 py-2 px-4 rounded-md">Download</button>
+      </div>
     </main>
   );
 }
